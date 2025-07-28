450fa9c5b8429217b6e6587d0fd218f88a8a319d
Gabriel Sampaio de Barros <gabriel.barros@al.sebrae.com.br>
2025-07-28 13:37:13 -0300

    feat: new dto result file

---
+++ src/core/models/dto/dto_result.py
@@ -0,0 +1,34 @@
+from typing import (
+    Optional, 
+    Any,
+    List
+)
+
+from pydantic import (
+    BaseModel, 
+    model_validator
+)
+
+from core.models.enum import dto_status
+
+class Result(BaseModel):
+    status: dto_status.Status
+    why_error: Optional[str] = None
+    result: Optional[Any] = None
+
+    @model_validator(mode='after')
+    def validate_why_error(self):
+        if self.status == dto_status.Status.FAILED:
+            if self.why_error is None:
+                raise ValueError('why_error is required when status is FAILED')
+        return self
+
+class ActionResult(BaseModel):
+    result: Result
+    retries: int
+
+class ComposedActionResult(BaseModel):
+    result: Result
+    report: List[ActionResult]
+
+   
