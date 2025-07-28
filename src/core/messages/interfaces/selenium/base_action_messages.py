c6d503b6c95772c78ab4339bc0c683a6fd22eba0
Gabriel Sampaio de Barros <gabriel.barros@al.sebrae.com.br>
2025-07-28 14:28:58 -0300

    feat: new messages module

---
+++ src/core/messages/interfaces/selenium/base_action_messages.py
@@ -0,0 +1,3 @@
+
+
+ALL_ATTEMPTS_FAILED_MESSAGE = 'All attempts failed. Check the attempts list for more details'
---
+++ src/core/messages/validation_messages.py
@@ -0,0 +1,6 @@
+
+
+
+
+VARIABLE_MUST_BE_A_WEBDRIVER_INSTANCE = 'The {variable_name} must be a WebDriver instance'
+VARIABLE_MUST_BE_AN_INTEGER_GREATER_THAN_0 = 'The {variable_name} must be an integer greater than 0'
