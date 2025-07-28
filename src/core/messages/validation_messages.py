efc4db9c5843b9f0b932d6545ca93a1ea1372ca3
Gabriel Sampaio de Barros <gabriel.barros@al.sebrae.com.br>
2025-07-28 15:33:16 -0300

    feat: new validation messages

--- src/core/messages/validation_messages.py
+++ src/core/messages/validation_messages.py
@@ -1,6 +1,7 @@
 
 
-
-
 VARIABLE_MUST_BE_A_WEBDRIVER_INSTANCE = 'The {variable_name} must be a WebDriver instance'
 VARIABLE_MUST_BE_AN_INTEGER_GREATER_THAN_0 = 'The {variable_name} must be an integer greater than 0'
+VARIABLE_CANNOT_BE_EMPTY = 'The {variable_name} cannot be empty'
+ALL_ELEMENTS_OF_VARIABLE_MUST_BE_A_BASE_ACTION_INSTANCE = 'All elements of {variable_name} must be a BaseAction instance'
+SOME_ACTIONS_FAILED = 'Some actions failed, Check the report for more details'
